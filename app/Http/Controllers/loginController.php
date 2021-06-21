d64ef0c3fecb25922345bbebee4f32e4eacdc84f
HoangTon-99 <ducton1012gmail.com>
2021-06-22 00:56:34 +0700

    upload form login signup Signed-off-by: HoangTon-99 <ducton1012gmail.com>

--- app/Http/Controllers/loginController.php
+++ app/Http/Controllers/loginController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
+use Illuminate\Support\Facades\DB;
+use App\Models\User;
 
 class loginController extends Controller
 {
@@ -37,19 +38,48 @@ class loginController extends Controller
         return view('hoangton.register');
     }
     public function postregister(Request $request){
-        $request->validate([
+        // $request->validate([
+        //     'name' =>'required|min:6|max:30|alpha',
+        //     'email' =>'required|email',
+        //     'address' =>'required|min:5|max:300|',
+        //     'phone' =>'required|min:10|numeric',
+        //     'password' =>'required|confirmed|min:5|max:30',
+
+        // ]);
+        // $user = DB::table('(users)')->where('email',$request->email)->first();
+        // if(!$user){
+        //     $newUser = new User();
+        //     $newUser ->name = $request->name;
+        //     $newUser ->email = $request->email;
+        //     $newUser ->address = $request->address;
+        //     $newUser ->sex = $request->sex;
+        //     $newUser ->phone = $request->phone;
+        //     $newUser ->password = $request->password;
+        //     $newUser ->save();
+        //     return redirect()->route('hoangton.show')->with('message','Bạn đã tao tài khoản thành công,mời bạn đăng nhập');
+
+        // }else{
+        //     return redirect()->route('hoangton.showlogin')->with('message','Tài khoản đã tồn tại mời bạn đăng nhập');
+        // }
+
+        // $remember=$request->has('remember') ? true : false;
+        // if(Auth::attempt(['name'=>$request->name,'email'=>$request->email,'address'=>$request->address,'phone'=>$request->phone, 'password'=>$request->password,'confirmpass'=>$request->confirmpass],$remember)){
+        //     return redirect('/')->with('thongbao','Đăng kí Thành Công');
+        // } else{
+        //     return redirect()->back()->with('thongbao','Đăng Kí thất bại');
+        // }
+        $this->validate(request(), [
             'name' =>'required|min:6|max:30|alpha',
             'email' =>'required|email',
-            'address' =>'required|min:5|max:300|',
-            'phone' =>'required|min:10|numeric',
-            'password' =>'required|confirmed|min:5|max:30',
-            'confirmpass'=>'required|confirmed|min:5|max:30'
+           'address' =>'required|min:5|max:300',
+           'phone' =>'required|min:10|numeric',
+           'password' =>'required',
         ]);
-        $remember=$request->has('remember') ? true : false;
-        if(Auth::attempt(['name'=>$request->name,'email'=>$request->email,'address'=>$request->address,'phone'=>$request->phone, 'password'=>$request->password,'confirmpass'=>$request->confirmpass],$remember)){
-            return redirect('/')->with('thongbao','Đăng kí Thành Công');
-        } else{
-            return redirect()->back()->with('thongbao','Đăng Kí thất bại');
-        }
+
+        $user = User::create(request(['name', 'email','address','phone', 'password']));
+
+        auth()->login($user);
+
+        return redirect()->to('/');
     }
 }
